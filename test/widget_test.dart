6d6fb27d7e2275b2d0e02c6b2e6a5ed6f7f5713a
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2024-12-31 19:02:12 +0100

    fixed widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,9 +13,7 @@ import 'package:flutter_global_scaffold/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(
-      onBuild: () {},
-    ));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
